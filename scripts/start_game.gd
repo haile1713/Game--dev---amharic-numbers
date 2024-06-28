b046caddd98d6961a56c2f7f7824c09a8d7daeee
haile <haileleulfiseha@gmail.com>
2024-06-28 20:33:20 +0300

    Buttons fixed
    the start and quit buttons are all ready to go !!

--- scripts/start_game.gd
+++ scripts/start_game.gd
@@ -1,9 +1,7 @@
 extends Control
-# it doesn't work don't touch this
-func _on_start_button_pressed(): 
-	print("start")
+#its ok i fixed it 
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scences/map.tscn")
 
 func _on_quit_button_pressed():
-	print("quit")
 	get_tree().quit()
