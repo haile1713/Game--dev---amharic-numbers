0ebb74569c14a45412b1422e2860ff955979a910
fit-s-u-m <nehemiahmaths@gmail.com>
2024-06-30 11:23:19 +0300

    added score and game end and game over criteria

--- scripts/level1.gd
+++ scripts/level1.gd
@@ -4,6 +4,7 @@ func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scences/map.tscn")
 var card_numbers = {}
 var right_number;
+var score
 func _ready():
 	var cards = []
 	for i in grid.get_children():
@@ -12,11 +13,19 @@ func _ready():
 		card_numbers[str(i.name)] = button.name
 		print(button.name)
 	right_number = 1
+	score = 2
 
 func _handle_number_clicked(card):
 	var num = card_numbers[card].to_int()
-	print("number ",num, " touched ")
-	if(num == right_number):
+	print("number ",num, " touched ", "score: ", score)
+	if(right_number == 9): # finished the game
+		print("congradulation")
+	elif(num == right_number): # correct number touched
+		score+=2
 		right_number+=1
-	else:
-		print("game over")
+	else: # in correct
+		score -=1
+		print("not correct try again")
+		if score <=0:
+			print("Game over")
+	
